0c319846a36c5bdf83a6d9a7f44594435e9e13a3
morganlenihan <160782014+morganlenihan@users.noreply.github.com>
2024-05-01 15:12:25 -0400

    Delete contacts-app directory

--- contacts-app/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent,appConfig)
-  .catch((err) => console.error(err));
-
